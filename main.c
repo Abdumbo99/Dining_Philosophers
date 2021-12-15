3aa3a0e38909256cbbf864f3e9b6323e0030d0c9
Abdumbo99 <razoq1999@gmail.com>
2021-12-15 03:36:38 +0300

    done with uniform randomness

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "pthread.h"
 
 #define PHIL_NUM 5
@@ -7,25 +9,78 @@
 #define THINKING 2
 #define HUNGRY 1
 #define EATING 0
-#define MAX_SLEEP_TIME 5
-#define MAX_EATING_TIME 10
-struct phil {
-    int state;
-    int cond;
-};
+#define MAX_THINK_TIME 10
+#define MAX_EATING_TIME 5
+
+pthread_mutex_t mutex;
+pthread_cond_t cond[PHIL_NUM];
+int state[PHIL_NUM];
+int thread_id[PHIL_NUM];
 
 
+void test(int ind){
+    if (state[RIGHT_NEIGHBOR(ind)] != EATING && state[LEFT_NEIGHBOR(ind)]!= EATING
+        && state[ind] == HUNGRY ){
+        state[ind] = EATING;
+        pthread_cond_signal(&cond[ind]);
+    }
+}
+
+void pickUpFork(int ind){
+    pthread_mutex_lock(&mutex);
+    state[ind] = HUNGRY;
+    test(ind);
+    if (state[ind] != EATING)
+        pthread_cond_wait(&cond[ind], &mutex);
+    pthread_mutex_unlock(&mutex);
+}
+
+void putDownFork (int ind){
+    pthread_mutex_lock(&mutex);
+    state[ind] = THINKING;
+    test(RIGHT_NEIGHBOR(ind));
+    test(LEFT_NEIGHBOR(ind));
+    pthread_mutex_unlock(&mutex);
+}
+
+int generateUniRandom (int highRange){
+    double myRand = rand()/(1.0 + RAND_MAX); // uniform distribution
+    return myRand * highRange + 1;
+}
+static void* philosiphize(void * indPtr){
+    int duration;
+    int *index = (int*) indPtr;
+    int ind = * index;
+    while (1) {
+        srand(time(NULL) * ind);
+        duration =generateUniRandom(MAX_THINK_TIME);
+        printf("Philosopher number (%d) is thinking for (%d) seconds\n", ind ,duration);
+        sleep(duration);
+        pickUpFork(ind);
+        duration =generateUniRandom(MAX_EATING_TIME);
+        printf("Philosopher number (%d) is eating for (%d) seconds\n", ind ,duration);
+        sleep(duration);
+        putDownFork(ind);
+    }
+}
+
+void init (int ind){
+    state[ind] = THINKING;
+    thread_id[ind] = ind;
+    pthread_cond_init(&cond[ind],NULL);
+}
 int main() {
-    int count = 0;
-    pthread_mutex_lock(&count);
-    pthread_mutex_unlock(&count);
-    printf("The following lines are to make sure the macros work as intended\n");
-    int i =2;
-    printf("%d\n",RIGHT_NEIGHBOR(i));
-    printf("%d\n",LEFT_NEIGHBOR(i));
-    printf("%d\n",RIGHT_NEIGHBOR(4));
-    printf("%d\n",LEFT_NEIGHBOR(4));
-    printf("%d\n",RIGHT_NEIGHBOR(0));
-    printf("%d\n",LEFT_NEIGHBOR(0));
+    pthread_t tid[PHIL_NUM];
+
+    for (int i = 0; i < PHIL_NUM; i++){
+        init(i);
+    }
+    pthread_mutex_init(&mutex, NULL);
+    for (int i = 0; i < PHIL_NUM; i++){
+        pthread_create(&(tid[i]),NULL, philosiphize,(void*)&thread_id[i]);
+    }
+    for (int i = 0; i < PHIL_NUM; i++){
+        pthread_join((tid[i]), NULL);
+    }
     return 0;
 }
